02887dd78d1531be3316db96e7da49901a3e4b9e
anishmo99 <ani10sh@gmail.com>
2020-09-24 09:37:47 +0530

    Create datatypes.dart

---
+++ datatypes.dart
@@ -0,0 +1,27 @@
+main(){
+  int a = 100;
+  var b = 200;
+
+  print('Amount1: $a | Amount2 : $b\n');
+
+  double c = 100.32;
+  var d = 200.24;
+
+  print('Amount3: $c | Amount4 : $d\n');
+
+  String name1 = 'Anish';
+  var name2 = 'Mookherjee';
+
+  print('My name is $name1 $name2\n');
+
+  bool x = false;
+  var y = true;
+
+  print('isItTrue : $x | isItTrue : $y\n');
+
+  dynamic weakVariable = 100;
+  print('$weakVariable\n');
+
+  weakVariable = 'Dart Programming';
+  print('$weakVariable\n');
+}
