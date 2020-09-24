9c3fbf5b161db25f3aff372e1a64ecabdbd63b44
anishmo99 <ani10sh@gmail.com>
2020-09-24 09:37:36 +0530

    Create basic.dart

---
+++ basic.dart
@@ -0,0 +1,8 @@
+import 'dart:core'; // automatically imported in every dart program
+
+void main(){
+  var firstName = 'Anish';
+  String lastName = 'Mookherjee';
+  print(firstName + lastName);
+  print('hello world'); 
+}
