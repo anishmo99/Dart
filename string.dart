99921eba550900c1c696312b0f59cb5f9fb13560
anishmo99 <ani10sh@gmail.com>
2020-09-24 09:58:39 +0530

    Create string.dart

---
+++ string.dart
@@ -0,0 +1,14 @@
+main(){
+  var s1 = 'Hi';
+  var s2 = " Anish";
+  var s3 = 'Happy Engineer\'s Day';
+  var s4 = "Happy Engineer's Day";
+  
+  print(s1 + s2);
+  print(s3);
+  print(s4);
+
+// raw string
+  var s = r'in s raw string, not even \n gets special treatment';
+  print(s);
+}
