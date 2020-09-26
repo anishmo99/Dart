08e5152534c738330ef33212dd49aeb61f3dc3fe
anishmo99 <ani10sh@gmail.com>
2020-09-27 00:36:37 +0530

    Create conditional.dart

---
+++ conditional.dart
@@ -0,0 +1,28 @@
+main() {
+  // same as cpp, java
+  int n = 1;
+  if (n == 0)
+    print('the number is 0');
+  else if (n == 1)
+    print('the number is 1');
+  else
+    print('the number is neither 0 nor 1');
+
+  int m = 0;
+
+  switch (m) {
+    case 0:
+      print('m is 0');
+      break;
+    case 1:
+      print('m is 1');
+      break;
+    default:
+      print('m is confused');
+  }
+}
+
+// OUTPUT
+
+// the number is 1
+// m is 0
