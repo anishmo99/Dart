0dd6985583a2551d0c2f3522150f93a7cab47470
anishmo99 <ani10sh@gmail.com>
2020-09-28 01:57:55 +0530

    Update map.dart

--- map.dart
+++ map.dart
@@ -7,5 +7,19 @@ main() {
     'third': 3
   };
 
+  map['third'] = 5; // updating existing value of key
   print(map['third']);
+
+  map['fourth'] = 4; // adding new element to the map
+  print(map['fourth']);
+
+  var gifts = Map();
+  gifts[0] = 'hello';
+  gifts[1] = 'hi';
+  gifts[2] = 'bye';
 }
+
+// OUTPUT
+
+// 5
+// 4
